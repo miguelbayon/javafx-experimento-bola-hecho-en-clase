9205ad9d57d4d1a73fca719f0b89e5b6a2c8193a
Miguel Bayon <miguel.bayon@iessanandres.com>
2017-05-18 13:30:54 +0200

    Ahora la bola se mueve sola en diagonal

--- ExperimentoBola.java
+++ ExperimentoBola.java
@@ -4,30 +4,39 @@ import javafx.scene.Scene;
 import javafx.scene.Group;
 import javafx.scene.shape.Circle;
 import javafx.scene.paint.*;
-
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
 
 public class ExperimentoBola extends Application
 {
-  public static void main(String[] args){
-    launch(args);
-  }
-  
-  @Override
-  public void start(Stage escenario)
-  {
-  	Group contenedor = new Group();
-    
-    Circle circulo = new Circle();
- 	circulo.setFill(Color.RED);  
-    circulo.setRadius(20);
-    circulo.setCenterX(250);
-    circulo.setCenterY(250);
-    contenedor.getChildren().add(circulo);
-    
-    Scene escena = new Scene(contenedor, 500, 500);
-    escenario.setScene(escena);
-    escenario.show();
-		
-  }
-  
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage escenario)
+    {
+        Group contenedor = new Group();
+
+        Circle circulo = new Circle();
+        circulo.setFill(Color.RED);  
+        circulo.setRadius(20);
+        circulo.setCenterX(250);
+        circulo.setCenterY(250);
+        contenedor.getChildren().add(circulo);
+
+        Scene escena = new Scene(contenedor, 500, 500);
+        escenario.setScene(escena);
+        escenario.show();
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0.017), event -> {
+                        circulo.setTranslateX(circulo.getTranslateX() + 2);
+                        circulo.setTranslateY(circulo.getTranslateY() + 2);
+                    }));                
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();      
+
+    }
+
 }
